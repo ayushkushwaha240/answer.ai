1f4bbe8bd3a5648faacdb0ec15895b0468d2c978
Ayush Kushwaha <148432773+ayushkushwaha240@users.noreply.github.com>
2024-08-26 10:05:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import logging
+
+# Replace with the module where your model is defined
+from model import load_models, answer
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+models = load_models()
+
+class Query(BaseModel):
+    question: str
+    # You can use this to pass site context if needed
+    # site_context: str
+
+@app.post("/ask/")
+async def ask_question(query: Query):
+    try:
+        ans = answer(query.question, models['tools'], models['qa'], models['ranker'])
+        return {"answer": ans}
+    except Exception as e:
+        logging.error(f"Exception occurred: {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+@app.get('/')
+def running():
+    return {"status": "working"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
